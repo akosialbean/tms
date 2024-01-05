a36ac51029d6b9d27d3256c3d31cd93af3677eec
akosialbean <alvincastor30@gmail.com>
2024-01-05 16:34:40 +0800

    Disabled autlogin after registration

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -1,5 +1,80 @@
 <?php
 
+// namespace App\Http\Controllers\Auth;
+
+// use App\Http\Controllers\Controller;
+// use App\Providers\RouteServiceProvider;
+// use App\Models\User;
+// use Illuminate\Foundation\Auth\RegistersUsers;
+// use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Validator;
+
+// class RegisterController extends Controller
+// {
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Register Controller
+//     |--------------------------------------------------------------------------
+//     |
+//     | This controller handles the registration of new users as well as their
+//     | validation and creation. By default this controller uses a trait to
+//     | provide this functionality without requiring any additional code.
+//     |
+//     */
+
+//     use RegistersUsers;
+
+//     /**
+//      * Where to redirect users after registration.
+//      *
+//      * @var string
+//      */
+//     protected $redirectTo = RouteServiceProvider::LOGIN;
+
+//     /**
+//      * Create a new controller instance.
+//      *
+//      * @return void
+//      */
+//     public function __construct()
+//     {
+//         $this->middleware('guest');
+//     }
+
+//     /**
+//      * Get a validator for an incoming registration request.
+//      *
+//      * @param  array  $data
+//      * @return \Illuminate\Contracts\Validation\Validator
+//      */
+//     protected function validator(array $data)
+//     {
+//         return Validator::make($data, [
+//             'name' => ['required', 'string', 'max:255'],
+//             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+//             'password' => ['required', 'string', 'min:8', 'confirmed'],
+//             'role' => ['required'],
+//         ]);
+//     }
+
+//     /**
+//      * Create a new user instance after a valid registration.
+//      *
+//      * @param  array  $data
+//      * @return \App\Models\User
+//      */
+//     protected function create(array $data)
+//     {
+//         return User::create([
+//             'name' => $data['name'],
+//             'email' => $data['email'],
+//             'password' => Hash::make($data['password']),
+//             'role' => $data['role'],
+//         ]);
+//     }
+// }
+
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
@@ -8,45 +83,21 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::LOGIN;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -57,12 +108,6 @@ class RegisterController extends Controller
         ]);
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
     protected function create(array $data)
     {
         return User::create([
@@ -71,5 +116,22 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
             'role' => $data['role'],
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        // Comment out the following line to prevent automatic login
+        // $this->guard()->login($user);
+
+        if ($response = $this->registered($request, $user)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 201): redirect('/login');  // Redirect to /login page
     }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('index');
 
+// Auth::routes(['register' => false]);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
