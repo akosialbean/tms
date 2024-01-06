02ff7ac8521a5efd33c66423b3943b222e42cebf
akosialbean <alvincastor30@gmail.com>
2024-01-06 15:50:38 +0800

    Created separate view for admin and user

--- backend/app/Http/Controllers/Task/TaskController.php
+++ backend/app/Http/Controllers/Task/TaskController.php
@@ -7,5 +7,5 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    
 }
--- backend/app/Http/Controllers/User/UserController.php
+++ backend/app/Http/Controllers/User/UserController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function getUsers(){
+        $data = User::orderby('name', 'asc');
+    }
 }
